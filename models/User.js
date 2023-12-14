410e63b3b03bfbb0a10657a4d68711bc5073de8a
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-12-14 04:46:43 -0700

    updated user model

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
-  // static async hashPassword(password) {
-  //   return bcrypt.hash(password, 10);
-  // }
+  static async hashPassword(password) {
+    return bcrypt.hash(password, 10);
+  }
 
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
@@ -38,27 +38,31 @@ User.init(
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isStrongPassword(value) {
-          const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-          if (!strongPasswordRegex.test(value)) {
-            throw new Error(
-              'The password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.'
-            );
-          }
-        },
-      },
+       validate: {
+        min: 8,
+        max: 10
+       },
+      // {
+      //   isStrongPassword(value) {
+      //     const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      //     if (!strongPasswordRegex.test(value)) {
+      //       throw new Error(
+      //         'The password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.'
+      //       );
+      //     }
+      //   },
+      // },
     },
   },
   {
     hooks: {
       beforeCreate: async (newUserData) => {
-        newUserData.password = await User.hashPassword(newUserData.password, 8);
+        newUserData.password = await User.hashPassword(newUserData.password);
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
         if (updatedUserData.changed('password')) {
-          updatedUserData.password = await User.hashPassword(updatedUserData.password, 8);
+          updatedUserData.password = await User.hashPassword(updatedUserData.password);
         }
         return updatedUserData;
       },
