61cc2096449a7a522389c0fa59c857d7c2e72214
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-12-14 00:48:08 -0700

    updated auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,34 @@
-const withAuth = (req, res, next) => {
-  // If the user is not logged in, redirect the request to the login route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
-module.exports = withAuth;
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    // Check if the user exists and the password is correct
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.logged_in = true;
+      req.session.user_id = user.id;
+
+      // Redirect to the homepage or a dashboard after successful login
+      res.redirect('/');
+    } else {
+      // If the user does not exist or wrong password
+      res.render('login', { error: 'Invalid email or password' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
+
+
